c0a821cb82006aa9f2de74e00f882026c84cda56
Lucien Carroll <lucien@discurs.us>
2022-05-02 22:53:27 -0700

    docs are wrong?

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def client(audio_data: np.array, sample_rate: int, use_scorer=False):
     output_audio = _convert_audio(audio_data, sample_rate)
 
     out_prob, score, index, text_lab = lang_classifier.classify_batch(
-        lang_classifier.normalizer(output_audio.getvalue(), sample_rate)
+        lang_classifier.audio_normalizer(output_audio.getvalue(), sample_rate)
     )
 
     fin = wave.open(output_audio, 'rb')
